dbdf9878c906b1e9e17c7d310826a246010773e7
Alex Perez <alexperez689@gmail.com>
2019-03-19 10:23:31 -0400

    comments removed from router, script + scss added

--- src/router.js
+++ src/router.js
@@ -19,15 +19,12 @@ export default new Router({
       // route level code-splitting
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue'),
+      component: () => import('./views/About.vue'),
     },
     {
       path: '/projects',
       name: 'projects',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/Projects.vue'),
+      component: () => import('./views/Projects.vue'),
     },
   ],
 });
--- src/views/Projects.vue
+++ src/views/Projects.vue
@@ -1,12 +1,10 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+  <div class="projects-container"></div>
 </template>
 
 <script>
 export default {
-  name: 'about',
+  name: 'projects',
 };
 </script>
 
